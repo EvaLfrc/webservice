6d67deaa8563e3b4afd87a9e9f40f4de4ed07e4f
EvaLfrc <maudeva62@gmail.com>
2024-03-17 22:52:00 +0100

    creation de deux web services

---
+++ loginapi.php
@@ -0,0 +1,37 @@
+<?php
+// Connexion à la base de données
+$connexion = mysqli_connect("localhost", "root", "0000", "mgsb");
+
+// Récupération des paramètres de la requête GET
+$username = $_GET['username'];
+$password = $_GET['mdpapi'];
+
+// Vérification de la connexion à la base de données
+if ($connexion) {
+    // Requête SQL pour récupérer le mot de passe associé à l'utilisateur
+    $sqlrequest = sprintf("SELECT mdpapi FROM users WHERE username = '%s'", $username);
+    $resultat = mysqli_query($connexion, $sqlrequest);
+
+    // Vérification de la requête SQL
+    if ($resultat) {
+        // Définition du type de contenu de la réponse comme JSON
+        header("Content-Type: JSON");
+
+        // Vérification du mot de passe
+        if ($password == $resultat->fetch_assoc()['mdpapi']) {
+            // Génération d'un token sécurisé
+            $token = bin2hex(random_bytes(16));
+
+            // Mise à jour du token dans la base de données
+            $sqlrequest = sprintf("UPDATE users SET cleapi ='%s' WHERE username = '%s'", $token, $username);
+            mysqli_query($connexion, $sqlrequest);
+
+            // Envoi du token encodé en JSON comme réponse
+            echo json_encode($token, JSON_PRETTY_PRINT);
+        }
+    }
+} else {
+    // En cas d'échec de la connexion à la base de données
+    echo json_encode("Echec de la connexion, veuillez réessayer", JSON_PRETTY_PRINT);
+}
+?>
---
+++ readficheapi.php
@@ -0,0 +1,71 @@
+<?php
+// Connexion à la base de données
+$connexion = mysqli_connect("localhost", "root","0000", "mgsb");
+
+// Récupération des paramètres de la requête GET
+$token = $_GET['token'];
+$ficheid = $_GET['ficheid'];
+
+// Requête pour récupérer la cleapi de la table users
+$sqlrequest = 'SELECT cleapi FROM users';
+$results = mysqli_query($connexion, $sqlrequest);
+
+// Parcours des résultats pour trouver la correspondance avec le token
+while ($row = $results->fetch_array()) {
+    if ($token == $row['cleapi']) {
+        // Récupération de l'ID utilisateur associé au token
+        $sqlrequest = sprintf("SELECT user_id FROM fichefrais WHERE id = '%s'", $ficheid);
+        $userid = mysqli_query($connexion, $sqlrequest)->fetch_assoc();
+        
+        // Récupération de l'ID utilisateur associé au token
+        $sqlrequest = sprintf("SELECT id FROM users WHERE cleapi = '%s'", $token);
+        $useridtoken = mysqli_query($connexion, $sqlrequest)->fetch_assoc();
+
+        // Vérifie que l'ID utilisateur de la fiche correspond à celui du token
+        if ($userid['user_id'] == $useridtoken['id']) {
+            // Récupération des informations de la fiche de frais
+            $sqlrequest = sprintf("SELECT * FROM fichefrais WHERE id = '%s'", $ficheid);
+            $infofichefrais = mysqli_query($connexion, $sqlrequest)->fetch_assoc();
+            
+            // Récupération des lignes de frais forfaitaires associées à la fiche
+            $sqlrequest = sprintf("SELECT LigneFraisForfait_id FROM fichefrais_lignefraisforfait WHERE FicheFrais_id = '%s'", $ficheid);
+            $idslignefraisforfait = mysqli_query($connexion, $sqlrequest);
+            $ids = array();
+            while ($row2 = $idslignefraisforfait->fetch_assoc()) {
+                $ids[] = $row2['LigneFraisForfait_id'];
+            }
+            $idstring = implode(",", $ids);
+            $sqlrequest = "SELECT * FROM lignefraisforfait WHERE id IN ($idstring)";
+            $lignefraisforfaitresults = mysqli_query($connexion, $sqlrequest);
+            $lignefraitforfait_tableau = array();
+            while ($row2 = $lignefraisforfaitresults->fetch_assoc()) {
+                $lignefraitforfait_tableau[] = $row2;
+            }
+            
+            // Récupération des lignes de frais hors forfait associées à la fiche
+            $sqlrequest = sprintf("SELECT LigneFraisHorsForfait_id FROM fichefrais_lignefraishorsforfait WHERE FicheFrais_id = '%s'", $ficheid);
+            $idslignefraishorsforfait = mysqli_query($connexion, $sqlrequest);
+            $ids = array();
+            while ($row2 = $idslignefraishorsforfait->fetch_assoc()) {
+                $ids[] = $row2['LigneFraisHorsForfait_id'];
+            }
+            $idstring = implode(",", $ids);
+            $sqlrequest = "SELECT * FROM lignefraishorsforfait WHERE id IN ($idstring)";
+            $lignefraishorsforfaitresults = mysqli_query($connexion, $sqlrequest);
+            $lignefraithorsforfait_tableau = array();
+            while ($row2 = $lignefraishorsforfaitresults->fetch_assoc()) {
+                $lignefraithorsforfait_tableau[] = $row2;
+            }
+            
+            // Stockage des résultats dans un tableau
+            $resultats = array();
+            $resultats['ficheinfo'] = $infofichefrais;
+            $resultats['ligneforfait'] = $lignefraitforfait_tableau;
+            $resultats['lignehorsforfait'] = $lignefraithorsforfait_tableau;
+            
+            // Conversion en JSON et affichage
+            echo json_encode($resultats, JSON_PRETTY_PRINT);
+        }
+    }
+}
+?>
